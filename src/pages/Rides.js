6a0e2c7916058d9134abc571a962733af81b35cc
oxfordgroupza-hue <oxfordgroupza@gmail.com>
2025-10-19 21:50:27 +0200

    Create Rides.js component

---
+++ src/pages/Rides.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Rides() {
+  return (
+    <div>
+      <h2>Book a Ride</h2>
+      <p>Request your ride here. (Feature coming soon!)</p>
+    </div>
+  );
+}
+
+export default Rides;
