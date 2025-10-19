1c29c86bbdb8bbc0d11cc6dcc9dcba10b2b0665f
oxfordgroupza-hue <oxfordgroupza@gmail.com>
2025-10-19 21:50:50 +0200

    Create Profile.js in src/pages

---
+++ src/pages/Profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div>
+      <h2>Your Profile</h2>
+      <p>View and edit your personal information here.</p>
+    </div>
+  );
+}
+
+export default Profile;
