c4286ca14ebc88acc99c4eb43511eb7d1590e04c
oxfordgroupza-hue <oxfordgroupza@gmail.com>
2025-10-19 21:50:11 +0200

    Create Messages.js component

---
+++ src/pages/Messages.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Messages() {
+  return (
+    <div>
+      <h2>Messages</h2>
+      <p>Chat with matches and friends. (Feature coming soon!)</p>
+    </div>
+  );
+}
+
+export default Messages;
