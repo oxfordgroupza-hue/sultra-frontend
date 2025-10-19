7b65d2acf9192edc6a1206b745cdfdbc30400575
oxfordgroupza-hue <oxfordgroupza@gmail.com>
2025-10-19 21:50:17 +0200

    Create Stay.js component for booking accommodations

---
+++ src/pages/Stay.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Stay() {
+  return (
+    <div>
+      <h2>Book a Stay</h2>
+      <p>Find and book accommodations. (Feature coming soon!)</p>
+    </div>
+  );
+}
+
+export default Stay;
