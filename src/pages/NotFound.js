a47c557d50b3d3478b5a3c5b1ec4ebfdf81143a7
oxfordgroupza-hue <oxfordgroupza@gmail.com>
2025-10-19 21:49:42 +0200

    Create NotFound.js component

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div>
+      <h2>404 - Page Not Found</h2>
+      <p>Sorry, that page does not exist.</p>
+    </div>
+  );
+}
+
+export default NotFound;
