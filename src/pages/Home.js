b6c81b0c8dac552fb30b51b34e3e021c3e4a94bd
oxfordgroupza-hue <oxfordgroupza@gmail.com>
2025-10-19 21:50:57 +0200

    Create Home.js component

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to Sultra</h2>
+      <p>Your all-in-one rides, stay, wallet, and social platform.</p>
+      <p>
+        <a href="/login">Login</a> | <a href="/register">Register</a>
+      </p>
+    </div>
+  );
+}
+
+export default Home;
