bb34d2b73dcff66be5d82965d83b13a38d9064fb
oxfordgroupza-hue <oxfordgroupza@gmail.com>
2025-10-19 21:50:22 +0200

    Create Wallet component

---
+++ src/pages/Wallet.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Wallet() {
+  return (
+    <div>
+      <h2>Your Wallet</h2>
+      <p>Top up, withdraw, and view transactions. (Feature coming soon!)</p>
+    </div>
+  );
+}
+
+export default Wallet;
